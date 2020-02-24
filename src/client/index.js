4eda525a909031ca7fa2e255e4dae441bffe2b16
Michael Bigelow <procureeminence@gmail.com>
2020-02-24 02:20:30 -0500

    feat: Added initial app styling & imported via index.js

--- src/client/index.js
+++ src/client/index.js
@@ -3,5 +3,9 @@
 
 /* Import SCSS styles */
 import './styles/reset.scss';
+import './styles/base.scss';
+import './styles/header.scss';
+import './styles/trip-entry.scss';
+import './styles/form.scss';
 
 /* Export JS functions */
