633e7a981984dd431acb36f6b45d389289d44709
Michael Bigelow <procureeminence@gmail.com>
2020-02-25 13:53:14 -0500

    feat: Added date format validation
    The regex used in this code was referenced from https://www.w3resource.com/javascript/form/javascript-date-validation.php but was modified to suit the needs of the travel app input. Originally this regex checked for a dd/mm/yyyy format but will now look for a mm/dd/yyyy format.

--- src/client/js/inputValidation.js
+++ src/client/js/inputValidation.js
@@ -11,6 +11,20 @@ const validDestination = ( destination ) => {
 const validDate = ( date ) => {
 
 	let valid = false;
+	let currentDate = new Date();
+	const dateRegExp = /^(0[1-9]|1[012])[\/\-](0[1-9]|[12][0-9]|3[01])[\/\-]\d{4}$/; // Date format mm/dd/yyyy.
+	
+	if( dateRegExp.test( date ) ){
+
+		// TODO: validation code
+
+		valid = true;
+
+	}else{
+
+		alert( 'Please enter a valid date format.' );
+
+	}
 
 	return valid;
 
