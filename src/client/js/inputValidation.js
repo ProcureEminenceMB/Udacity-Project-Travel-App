3b22326b54b48d77ec7fe211ed02bd521ef34dc7
Michael Bigelow <procureeminence@gmail.com>
2020-02-25 13:46:16 -0500

    chore: Defined empty helper functions for validating user input

---
+++ src/client/js/inputValidation.js
@@ -0,0 +1,17 @@
+const validDestination = ( destination ) => {
+
+	let valid = false;
+
+	// TODO: validation code
+
+	return valid;
+
+}
+
+const validDate = ( date ) => {
+
+	let valid = false;
+
+	return valid;
+
+}
